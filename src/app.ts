528f892cd0f9f468e8b607da83ab9b885e8f3705
Chekwube <chekwube@rubikpay.net>
2021-05-22 11:38:38 +0100

    Make project base setup=> setup database, error handler, logger, environment variable

---
+++ src/app.ts
@@ -0,0 +1,80 @@
+process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
+
+import 'reflect-metadata';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import compression from 'compression';
+import { createConnection } from 'typeorm';
+import Routes from '@interfaces/route.interface';
+import errorMiddleware from '@middlewares/error.middleware';
+import { logger, stream } from '@utils/logger';
+import { dbConnection } from './databases';
+
+class App {
+  public app: express.Application;
+  public port: string | number;
+  public env: string;
+
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.env = process.env.NODE_ENV || 'development';
+
+    this.connectToDatabase();
+    this.initializeMiddlewares();
+    this.initializeRoutes(routes);
+    this.initializeErrorHandling();
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      logger.info(`=================================`);
+      logger.info(`======= ENV: ${this.env} =======`);
+      logger.info(`🚀 App listening on the port ${this.port}`);
+      logger.info(`=================================`);
+    });
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  private connectToDatabase() {
+    if (this.env !== 'test') {
+      createConnection(dbConnection);
+    }
+  }
+
+  private initializeMiddlewares() {
+    if (this.env === 'production') {
+      this.app.use(morgan('combined', { stream }));
+      this.app.use(cors());
+    } else {
+      this.app.use(morgan('dev', { stream }));
+      this.app.use(cors({ origin: true, credentials: true }));
+    }
+
+    this.app.use(hpp());
+    this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+  }
+
+  private initializeRoutes(routes: Routes[]) {
+    routes.forEach(route => {
+      this.app.use('/api/v1', route.router);
+    });
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
+}
+
+export default App;
